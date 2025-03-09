bcafac2a71bf846f5c9edbc2d710b5b11e825f44
prince <thecloudguru2023@gmail.com>
2025-03-09 09:16:58 -0500

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen Ai for Everyone :) </h1>
+    <h1>Welcome to AI Agents World  </h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
@@ -35,3 +35,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+""
